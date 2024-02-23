65f6767ed4092a542e9c710f27d7ec0c4be73b69
Coreykauai <corey@student.dev-island.org>
2024-02-22 17:53:41 -1000

    more pic updates

--- src/fakeData.js
+++ src/fakeData.js
@@ -2,7 +2,8 @@ export const fakemovies = [
   {
     title: "The Lords of Dogtown",
     sumry: "The history of life.",
-    poster: "src/assets/moviePics/dogt.jpeg",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg",
     details: "PG-13 ・ 2005 ・ 1h47m ・ Banger ",
   },
 
@@ -10,14 +11,16 @@ export const fakemovies = [
     title: "Blue Crush",
     sumry:
       "Nothing gets between Anne Marie (Kate Bosworth) and her board. Living in a beach shack with three roommates including her rebellious younger sister, she is up before dawn every morning to conquer the waves and count the days until the Rip Masters surf competition. Anne Marie finds all she needs in the adrenaline-charged surf scene ... until pro quarterback Matt Tollman (Matthew Davis) comes along. Like it or not, Anne Marie starts losing her balance - and finding it - as she falls for Matt.",
-    poster: "src/assets/moviePics/bc.webp",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BYzYwZDQwNmEtMDIyNy00NjkzLWJjYTctNTYwNjI2MTBhMzEzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
     details: "PG-13 ・ 2002 ・ 1h50m ・ Surf Films ",
   },
   {
     title: "The Fast and the Furious",
     sumry:
       "Dominic Toretto (Vin Diesel) enjoys the adrenaline of street car racing and his fans treat him like a rock star. After a blazing encounter with the ruthless Johnny Tran, Dom decides to take Brian (Paul Walker), a newcomer to street racing, under his wing. Dom's sister Mia sees something she likes in Brian, too. Trouble is, neither of them realize he's an undercover cop, and Dominic and his rival Johnny Tran are both the prime suspects in a case involving dirty money and big-rig hijacking.",
-    poster: "src/assets/moviePics/fastass.avif",
+    poster:
+      "https://m.media-amazon.com/images/I/512kXn+IclL._AC_UF894,1000_QL80_.jpg",
     details: "PG-13 ・ 2001 ・ 1h46m ・ GoFast ",
   },
 ];
