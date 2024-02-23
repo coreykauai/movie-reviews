6ea463eaaca13c70e35d0795e6ea3da77cbba699
Coreykauai <corey@student.dev-island.org>
2024-02-22 17:46:21 -1000

    pics wasnt working from files

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const movies = [
   {
     title: "A Few Goodmen",
     sumry: `"Featuring John Goodman as 'John Goodman', John Goodman as 'John Goodman' and making his feature debut, John Goodman as 'John Goodman', in this all out thriller!"  `,
-    poster: "src/assets/moviePics/goodman.jpeg",
+    poster: "https://imgur.com/NTjhH.jpg",
     genere: "upcoming",
     details: "R ・ 1991 ・ 1h56m ・ Blockbuster ",
   },
@@ -18,7 +18,7 @@ export const movies = [
   {
     title: "Family Guy",
     sumry: `"The Movie you've been waiting for your entire life. Family Guy The Movie!"`,
-    poster: "src/assets/moviePics/familywhy.png",
+    poster: "https://i.redd.it/u2xhucddce4a1.jpg",
     genere: "fakemovie",
     details: "PG-13 ・ 2028 ・ 1h59m ・ Comedy ",
   },
@@ -26,7 +26,8 @@ export const movies = [
   {
     title: "Squird",
     sumry: `-"Here Lies SQUIDWARD'S HOPES and DREAMS"`,
-    poster: "src/assets/moviePics/squidy.png",
+    poster:
+      "https://preview.redd.it/thanks-i-hate-the-squidward-movie-v0-lz68l2snt6h91.png?width=640&crop=smart&auto=webp&s=b1d675d739069f4380cc944fcc70258b535f10ef",
     genere: "fakemovie",
     details: "R ・ 2025 ・ 4h56m ・ Action ",
   },
@@ -35,7 +36,8 @@ export const movies = [
     title: "Rich And Morty",
     sumry:
       "Interdimentonal Television Incorperated Brings you, Rick and Morty.",
-    poster: "src/assets/moviePics/ricky.png",
+    poster:
+      "https://i.pinimg.com/originals/aa/20/8b/aa208be36f93012cffdc2ea2f25ba35e.jpg",
     genere: "fakemovie",
     details: "R ・ 20023 ・ 1h26m ・ Comedy ",
   },
@@ -43,7 +45,8 @@ export const movies = [
   {
     title: "Merman",
     sumry: "wololollol wololollolo",
-    poster: "src/assets/moviePics/merman.jpeg",
+    poster:
+      "https://i.pinimg.com/736x/70/d5/2e/70d52eb76157783f78f2ace1cb5b9f2a.jpg",
     genere: "fakemovie",
     details: "PG ・ 2009 ・ 1h56m ・ Comedy ",
   },
@@ -52,7 +55,8 @@ export const movies = [
     title: "Scorcher IV",
     sumry:
       "Here we go again, again this summer the worlds in danger again and only one man can stop it, only this time it's different.",
-    poster: "src/assets/moviePics/tuggman.jpeg",
+    poster:
+      "https://i.pinimg.com/736x/46/e5/7d/46e57dbff7b7c583d137abdbef3c2c32.jpg",
     genere: "fakemovie",
     details: "PG-13 ・ 2008 ・ 2h27m ・ Comedy ",
   },
@@ -60,7 +64,8 @@ export const movies = [
   {
     title: "FRIENDS",
     sumry: `10 YEARS after the end of friends, They got back together to make "FRIENDS The Movie" !!! `,
-    poster: "src/assets/moviePics/frems.jpeg",
+    poster:
+      "https://substackcdn.com/image/fetch/w_600,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fpbs.substack.com%2Fmedia%2FC9C1WLfV0AAEemp.jpg",
     genere: "fakemovie",
     details: "R ・ 2024 ・ 2h38m ・ Comedy ",
   },
@@ -68,7 +73,8 @@ export const movies = [
   {
     title: "PAC-MAN",
     sumry: "you wana see this one",
-    poster: "src/assets/moviePics/pacman.jpeg",
+    poster:
+      "https://i.pinimg.com/originals/88/92/7f/88927fe5fb362ae6dcb88f84a425f472.jpg",
     genere: "fakemovie",
     details: "R ・ 2025 ・ 1h56m ・ Comedy, Action ",
   },
@@ -76,14 +82,16 @@ export const movies = [
     title: "Shaqshank",
     sumry:
       "The film tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence.",
-    poster: "src/assets/moviePics/shaq.jpeg",
+    poster:
+      "https://ih1.redbubble.net/image.238643499.1700/fposter,small,wall_texture,square_product,600x600.u2.jpg",
     genere: "fakemovie",
     details: "R ・ 1994 ・ 2h22m ・ Action/Crime ",
   },
   {
     title: "SPONGEWORLD",
     sumry: "ITS NOT WHAT YOU THINK",
-    poster: "src/assets/moviePics/spongeworld.png",
+    poster:
+      "https://www.shockya.com/news/wp-content/uploads/sb1-sht-spoofjurassicworld.jpg",
     genere: "fakemovie",
     details: "R ・ 2025 ・ 3h05m ・ Comedy ",
   },
@@ -91,7 +99,8 @@ export const movies = [
     title: "MAD YAX",
     sumry:
       "The series follows Max, initially a police officer in a future Australia which is experiencing societal collapse due to war, critical resource shortages, and ecocide. When his wife and child are murdered by a vicious biker gang, Max kills the gang in revenge and becomes a drifting loner in the Australian Wasteland.",
-    poster: "src/assets/moviePics/madyax.png",
+    poster:
+      "https://is.zobj.net/image-server/v1/images?r=fdJU0RV494e21tEpFyJc-vcsOZVBIn9fvvgXhL52qJfkRy0WE8lYA6Zir0WYkA0AXU-7hgNdeVR5LhK1WUGFuD5kJRfj8GcfGhPrZ32_yScEdNB3nXN0BT7IPfTiwKUfmgYBvjkclCFvlUazVvdxM2Oy0z3Ks6y2t0p0fvPlOsX640DHti7CIrIO0Usw-9nHLHwGNhXQfa9O4oYT6OVwPNAPWQkIjz8_LJiXfg",
     genere: "fakemovie",
     details: "R ・ 2024 ・ 1h47m ・ Action ",
   },
@@ -99,7 +108,8 @@ export const movies = [
     title: "The Big Short",
     sumry:
       "In 2008, Wall Street guru Michael Burry realizes that a number of subprime home loans are in danger of defaulting. Burry bets against the housing market by throwing more than $1 billion of his investors' money into credit default swaps. His actions attract the attention of banker Jared Vennett (Ryan Gosling), hedge-fund specialist Mark Baum (Steve Carell) and other greedy opportunists.",
-    poster: "src/assets/moviePics/thebigshort.png",
+    poster:
+      "https://3.bp.blogspot.com/-oARoZzw4BVY/VtB6Av2mulI/AAAAAAADCV0/wEYdH5FJ89E/s1600/zoo_poster_bigshort.jpg",
     genere: "fakemovie",
     details: "R ・ 2015 ・ 2h10m ・ Crime ",
   },
@@ -107,15 +117,9 @@ export const movies = [
     title: "Pineapple Express 2",
     sumry:
       "Red has become a drug lord in Ted Jones absence, but Woody Harrelson is threatening it all by promoting legal weed. Stoners Dale and Saul must assassinate Woody… or be assassinated themselves.",
-    poster: "src/assets/moviePics/pe2.jpeg",
+    poster:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8c1848db-b72d-4c19-921f-c6c3e03cc1c4/d2b5z52-45fbb727-84c8-45ae-80e4-f7ec1ef9a7d4.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhjMTg0OGRiLWI3MmQtNGMxOS05MjFmLWM2YzNlMDNjYzFjNFwvZDJiNXo1Mi00NWZiYjcyNy04NGM4LTQ1YWUtODBlNC1mN2VjMWVmOWE3ZDQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.wFqgwFjbbPA3unKAn28xHOLH_kRQDq-y-1XSbms5anQ",
     genere: "fakemovie",
     details: "R ・ 2025 ・ 1h50m ・ Comedy ",
-  },
-  {
-    title: "Gaurdians of the Garden",
-    sumry: "Who's watching your Garden??  ..who even has a garden",
-    poster: "src/assets/moviePics/nomes.png",
-    genere: "fakemovie",
-    details: "R ・ 2024 ・ 1h56m ・ Comedy ",
   },
 ];
